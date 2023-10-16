30100da5d9dee1400ab1807fa89c2e2cddb5c6fd
Wenxuan Guo <wg2361@columbia.edu@users.noreply.github.com>
2023-10-16 01:25:51 -0400

    add optimization of s space

--- pixel_loss.py
+++ pixel_loss.py
@@ -1,57 +1,35 @@
 import os, sys
 
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import re
 import argparse
 from tqdm import tqdm
 
-import numpy as np
-from numpy import arccos
-from numpy.linalg import norm
 import torch
 import torch.nn as nn
 
-from stylegan3 import legacy
-
-from model_zoo import VGG16_BFM_128
-from stimulus_optimization.activation import get_layerwise_activations
+from stylegan3.load_models import load_stylegan
 from stimulus_optimization.generator_objects import ModelParallel
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", type=int, required=True)
-parser.add_argument("--optimized", type=str, required=True)
-parser.add_argument("--truncation_psi", type=float, default=0.5)
-parser.add_argument("--num_seeds", type=int, default=5)
-args = parser.parse_args()
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--batch_size", type=int, required=True, default=16)
+argparser.add_argument(
+    "--img_resolution", type=int, required=True, default=128, choices=[128, 256, 1024]
+)
+argparser.add_argument("--aligned", default=False, action="store_true")
+args = argparser.parse_args()
 
 batch_size = args.batch_size
-optimized = args.optimized
-truncation_psi = args.truncation_psi
-
-
-def theta(v, w):
-    dot = (v @ w.T).diagonal()
-    return arccos(dot / (norm(v, axis=1) * norm(w, axis=1)))
-
-
-def normalized_sq_err(act_cur, act_target, act_target_norm):
-    feature_diff = act_cur.flatten(1) - act_target.flatten(1)
-    feature_diff_norm = torch.norm(feature_diff, p=2, dim=-1)
-    normalized_sq_err = feature_diff_norm / act_target_norm
-    return torch.mean(normalized_sq_err)
-
-
-def postprocess_torch(ims):
-    ims = ims.clone()
-    return (ims * 127.5 + 128).clamp(0, 255).to(torch.uint8).detach()
-
-
 model_device = torch.device("cuda:1")
 generator_device = [torch.device("cuda:{}".format(i)) for i in range(2)]
 param_device = generator_device[0]
 
-network_pkl = "/engram/nklab/face_proj/models/stylegan3/ffhq_128/00000-stylegan3-r-ffhq_128-gpus8-batch32-gamma0.5/network-snapshot-025000.pkl"
-with open(network_pkl, "rb") as f:
-    G = legacy.load_network_pkl(f)["G_ema"].to(param_device)
+G = load_stylegan(
+    img_resolution=args.img_resolution,
+    new_model=True,
+    cfg="stylegan3-r",
+    aligned=args.aligned,
+)
 
 z_dim = G.z_dim
 G_mapping = ModelParallel(
@@ -64,34 +42,16 @@ G_synthesis = ModelParallel(
 with torch.no_grad():
     z_target = torch.randn([batch_size, z_dim]).to(param_device)
     c_target = None
-    w_target = G_mapping(
-        z_target, c_target, truncation_psi=truncation_psi, truncation_cutoff=None
-    )
+    w_target = G_mapping(z_target, c_target, truncation_psi=0.5, truncation_cutoff=None)
     target_ims = G_synthesis(w_target, noise_mode="const", force_fp32=True)
-
-    face_model = VGG16_BFM_128(0)
-    face_model.load(device=model_device)
-    layer_idx = 34
-
-    act_target = get_layerwise_activations(
-        model=face_model,
-        layer_indices=[layer_idx],
-        ims=target_ims,
-        compression=None,
-        forward=True,
-        in_place=False,
-    )[0].to(param_device)
-    act_target.requires_grad_(False)
-    act_target_norm = torch.norm(act_target.flatten(1), p=2, dim=-1)
 
 optimized_types = ["z", "w", "s"]
 
-all_metamers = [target_ims]
-all_dissimilarities = []
-all_angles = []
+all_output = []
+all_loss = []
 
 for optimized in optimized_types:
-    for i_batch in range(args.num_seeds):
+    for i_batch in range(1):
         z_param = torch.randn([batch_size, z_dim]).to(param_device)
 
         if optimized == "w":
@@ -122,7 +82,7 @@ for optimized in optimized_types:
             optimized_params.values(), lr=lr
         )  # , betas=betas, eps=eps
         with torch.autograd.detect_anomaly():
-            for i in (pbar := tqdm(range(1000))):
+            for i in (pbar := tqdm(range(500))):
                 optimizer.zero_grad()
                 if optimized == "s":
                     output_ims = G_synthesis(
@@ -137,20 +97,11 @@ for optimized in optimized_types:
                         w_param = w_init.unsqueeze(1).repeat([1, G.num_ws, 1])
                     output_ims = G_synthesis(
                         w_param, noise_mode="const", force_fp32=True
-                    )
+                    ).to(param_device)
 
-                act_cur = get_layerwise_activations(
-                    model=face_model,
-                    layer_indices=[layer_idx],
-                    ims=output_ims,
-                    compression=None,
-                    forward=True,
-                    in_place=False,
-                )[0]
-                loss = normalized_sq_err(
-                    act_cur.to(param_device), act_target, act_target_norm
-                )
+                loss = torch.mean((target_ims.flatten(1) - output_ims.flatten(1)) ** 2)
                 pbar.set_description("loss: %0.4f" % (loss))
+
                 loss.backward()
                 optimizer.step()
                 if optimized == "z":
@@ -159,21 +110,9 @@ for optimized in optimized_types:
                     ).reshape(-1, 1)
 
         with torch.no_grad():
-            all_metamers.append(output_ims)
-            new_dissimilarities = torch.sum(
-                (
-                    act_cur.flatten(1).detach().cpu()
-                    - act_target.flatten(1).detach().cpu()
-                )
-                ** 2,
-                dim=1,
+            all_output.append(output_ims)
+            all_loss.append(
+                torch.mean((target_ims.flatten(1) - output_ims.flatten(1)) ** 2, dim=1)
+                .cpu()
+                .numpy()
             )
-            all_dissimilarities.append(new_dissimilarities.numpy())
-            if optimized == "z":
-                z_metamers = z_param.clone().detach().cpu().numpy()
-                z_targets = z_target.clone().detach().cpu().numpy()
-                angles = np.rad2deg(theta(z_metamers, z_targets))
-                all_angles.append(angles)
-
-metamers = torch.vstack(all_metamers)
-ims = postprocess_torch(metamers)
